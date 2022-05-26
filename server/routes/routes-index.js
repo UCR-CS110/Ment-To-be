e357ed52981fafd704b09254bf34a6630944c5a3
Michelle Chu <michellechu0131@gmail.com>
2022-05-26 02:02:35 -0700

    Profile edit, search, update end points

--- server/routes/routes-index.js
+++ server/routes/routes-index.js
@@ -1,6 +1,84 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("../passport/passport-index");
+const User = require('../models/user.js');
+
+/**
+* @return {array} - returns an array of all users stored in database
+*/
+router.get("/getUsers", function (req, res) {
+  User.find().lean().then(item => {
+    res.json(item)
+  });
+});
+
+/**
+* @queryparam {string} param email - unique id of users
+* @bodyparam {string} param body - all initial form data (see jeanette's doc)
+* @return {obj} - returns updated user 
+*/
+router.post("/:email/update", function (req, res) {
+  const email = req.params.email;
+  // const body = {
+  //   "birthday":"01/31/2000",
+  //   "gender": "female",
+  //   "location": "LA",
+  //   "bio": "hi",
+  //   "degree" : "cs bs",
+  //   "school" : "ucr",
+  //   "career_goal" : "swe",
+  //   "age" : "22",
+  //   "major" : ["computer science", "data_science"]
+  // }
+  const body = req.body;
+  const update = {"$set": {major: body.major, language: body.language, jobs: body.jobs, mentor_topics: body.mentor_topics, mentee_topics: body.mentee_topics, programming_languages: body.programming_languages,
+    birthday: body.birthday, gender: body.gender,location:body.location, bio: body.bio, school: body.school, degree: body.degree, career_goal: body.career_goal, age: body.career_goal}}
+  User.findOneAndUpdate({email: email}, update).exec(function(err, profile){
+    if(profile == undefined){
+      res.status(404).send("Profile not found.");
+    }else{
+      res.json(profile);
+    }
+  });
+});
+/**
+* @queryparam {string} param email - unique id of users
+* @bodyparam {number} param rating 
+* @return {obj} if user found - profile is sent back
+*               if user not found - 404
+*/
+router.post("/:email/addRating", function (req, res) {
+  const email = req.params.email;
+  const update = { $push: { ratings: 5} }
+  User.findOneAndUpdate({email: email}, update).exec(function(err, profile){
+    if(profile == undefined){
+      res.status(404).send("Profile not found.");
+    }else{
+      res.json(profile);
+    }
+  });
+});
+
+/**
+* @queryparam {string} param email - unique id of users
+* @return {obj} if user found - profile is sent back
+*                       if user not found - 404
+*/
+router.get("/:email", function (req, res) {
+  const email = req.params.email;
+  User.findOne({email: email}).exec(function(err, profile){
+    if(err){
+      res.send("Error:", error);
+    }else if(profile == undefined){
+      res.status(404).send("Profile not found.");
+    }else{
+      res.status(200);
+      res.send(profile);
+    }
+  });
+});
+
+
 
 router.get(
   "/auth/google",
@@ -36,6 +114,9 @@ router.get(
     res.redirect("http://localhost:3000/dashboard");
   }
 );
+
+
+
 
 //Protected Route.
 router.get("/auth/current-session", check_user_logged_in, (req, res) => {

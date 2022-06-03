a6bd55c5a3f39e9a7735f308448f37c7fc548018
Michelle Chu <michellechu0131@gmail.com>
2022-06-02 22:55:22 -0700

    Added getMentee function for matchmaking

--- server/routes/routes-index.js
+++ server/routes/routes-index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const passport = require("../passport/passport-index");
 const User = require('../models/user.js');
+const Topic = require('../models/topic.js');
+
+
+
 
 /**
 * @return {array} - returns an array of all users stored in database
@@ -13,48 +17,106 @@ router.get("/getUsers", function (req, res) {
 });
 
 /**
-* @queryparam {string} param email - unique id of users
-* @bodyparam {string} param body - all initial form data (see jeanette's doc)
-* @return {obj} - returns updated user 
+* @return {array} - matches a mentee to the Mentor based on topic. If the topic mentor has chose has no users signed up, a new Ment
+  "No mentee available" will be sent back.
 */
-router.post("/:email/update", function (req, res) {
+router.get("/:email/getMentee", async (req, res) => {
   const email = req.params.email;
-  // const body = {
-  //   "birthday":"01/31/2000",
-  //   "gender": "female",
-  //   "location": "LA",
-  //   "bio": "hi",
-  //   "degree" : "cs bs",
-  //   "school" : "ucr",
-  //   "career_goal" : "swe",
-  //   "age" : "22",
-  //   "major" : ["computer science", "data_science"]
-  // }
+  await User.findOne({email: email}).exec(async (err, profile) =>{
+    if(err){
+      console.log("400")
+      return res.status(400).json("Error:", error);
+    }else if(profile == undefined){
+      console.log("400")
+      return res.status(404).json("Profile not found.");
+    }else{
+      if(profile.mentor_profile_exists){
+        const mentor_topic = profile.mentor_profile.mentor_topic;
+        const matched_users = profile.matched_mentees;
+        await Topic.findOne({topic: mentor_topic}).exec(async(err, topic) =>{
+          const topic_mentees_arr = topic.mentees;
+          for(let j = 0; j < topic_mentees_arr.length; j++){
+            if(!matched_users.includes(topic_mentees_arr[j])){
+              var update = { $push: { matched_mentees: topic_mentees_arr[j] } };
+              await User.findOneAndUpdate({email: email}, update);
+              update = { $push: { matched_mentors: email } };
+              await User.findOneAndUpdate({email: topic_mentees_arr[j]}, update);
+              return res.status(200).json(topic_mentees_arr[j]);
+            }else{
+              var mentees = await User.find({ mentee_profile_exists: true}).exec();
+              for(let i = 0; i < mentees.length; i++){
+                if(!matched_users.includes(mentees[i]) && mentees[i].email != email){
+                  var update = { $push: { matched_mentees: mentees[i] } };
+                  await User.findOneAndUpdate({email: email}, update);
+                  update = { $push: { matched_mentors: email } };
+                  await User.findOneAndUpdate({email: mentees[i]}, update);
+                  return res.status(200).json(mentees[i])
+                }
+              }
+              return res.status(200).json("No mentee available");
+            }
+          }
+        })
+
+      }else{
+        return res.status(400).json("User is not a mentor and cannot be assigned a mentee.")
+      }
+    }
+  });
+});
+
+
+/**
+* @queryparam {string} param email - unique id of user
+* @bodyparam {string} param body - mentee form data (language(str), bio(str), year(str), career_goal(str), mentee_topic(str))
+* @return {obj} - returns updated user if success 
+*/
+router.post("/:email/updateMenteeProfile", function (req, res) {
+  const email = req.params.email;
   const body = req.body;
-  const update = {"$set": {major: body.major, language: body.language, jobs: body.jobs, mentor_topics: body.mentor_topics, mentee_topics: body.mentee_topics, programming_languages: body.programming_languages,
-    birthday: body.birthday, gender: body.gender,location:body.location, bio: body.bio, school: body.school, degree: body.degree, career_goal: body.career_goal, age: body.career_goal}}
+  const update = {"$set": { language: body.language, bio: body.bio, 'mentee_profile.mentee_year' : body.year
+  ,'mentee_profile.mentee_career_goal' : body.career_goal, 'mentee_profile.mentee_topic' : body.mentee_topic, mentee_profile_exists: true}}
   User.findOneAndUpdate({email: email}, update).exec(function(err, profile){
     if(profile == undefined){
       res.status(404).send("Profile not found.");
     }else{
-      res.json(profile);
+      res.status(200).json(profile);
+    }
+  });
+});
+
+/**
+* @queryparam {string} param email - unique id of user
+* @bodyparam {string} param body - mentor form data (language(str), bio(str), jobs(array[{company:str,job_title:str}]), expertise(str), mentor_topic(str))
+* @return {obj} - returns updated user if success 
+*/
+router.post("/:email/updateMentorProfile", function (req, res) {
+  const email = req.params.email;
+  const body = req.body;
+  const update = {"$set": { language: body.language, bio: body.bio, 'mentor_profile.mentor_jobs' : body.jobs
+  ,'mentor_profile.expertise' : body.expertise, 'mentor_profile.mentor_topic' : body.mentor_topic, mentor_profile_exists: true}}
+  User.findOneAndUpdate({email: email}, update).exec(function(err, profile){
+    if(profile == undefined){
+      res.status(404).send("Profile not found.");
+    }else{
+      res.status(200).json(profile);
     }
   });
 });
 /**
 * @queryparam {string} param email - unique id of users
-* @bodyparam {number} param rating 
+* @bodyparam {number} param rating (num)
 * @return {obj} if user found - profile is sent back
 *               if user not found - 404
 */
 router.post("/:email/addRating", function (req, res) {
   const email = req.params.email;
-  const update = { $push: { ratings: 5} }
+  const update = { $push: { 'mentor_profile.ratings': req.body.rating} }
   User.findOneAndUpdate({email: email}, update).exec(function(err, profile){
     if(profile == undefined){
       res.status(404).send("Profile not found.");
     }else{
-      res.json(profile);
+      res.status(200).send(profile);
     }
   });
 });
@@ -72,8 +134,7 @@ router.get("/:email", function (req, res) {
     }else if(profile == undefined){
       res.status(404).send("Profile not found.");
     }else{
-      res.status(200);
-      res.send(profile);
+      res.status(200).send(profile);
     }
   });
 });
